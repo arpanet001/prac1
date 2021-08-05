5f7521afcbf514512e6c97e4cd72feefdec56630
arpanet001 <sam.ndambuki08@gmail.com>
2021-08-05 14:12:16 +0300

    Prac1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace prac1
+{
+    class Program
+    {
+
+        //public function visible from outside the class
+        //void - Does not return any value
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello World");
+
+        }
+        static void Main(string[] args)
+        {
+            var t = new Program();
+            t.SayHello();
+
+            //Displays console window and awaits key entry eg. Enter
+            Console.ReadKey();
+            
+        }
+    }
+}
+
+//function x-tics :
+//access level - public private 
+//return value - void or int
+//Method name - sayhello()
